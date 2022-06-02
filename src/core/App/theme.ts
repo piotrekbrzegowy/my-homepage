a621bb892d44186c4707632c0019139e5ae37f65
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-06-02 17:24:19 +0200

    theme added

---
+++ src/core/App/theme.ts
@@ -0,0 +1,71 @@
+const colorNames = {
+    white: "#ffffff",
+    mineShaft: "#252525",
+    mineShaft72percent: "#252525B8",
+    slateGray: "#6E7E91",
+    mercury: "#E5E5E5",
+    iron30percent: "#D1D5DA4D",
+    iron10percent: "#D1D5DA1A",
+    scienceBlue: "#0366D6",
+    scienceBlue50percent: "#0366D680",
+    scienceBlue20percent: "#0366D633",
+    dodgerBlue: "#2188FF",
+    violet2percent: "#090A3305",
+}
+
+const media = {
+    mobileMax: 767,
+    tabletMax: 991
+}
+
+export const lightTheme = {
+    ...media,
+    color: {
+        textPrimary: colorNames.mineShaft,
+        textSecondary: colorNames.slateGray,
+        background: colorNames.mercury,
+        sectionBackground: colorNames.white,
+        sectionShadow: colorNames.violet2percent,
+        sectionTitleBorder: colorNames.iron30percent,
+        sectionBullet: colorNames.scienceBlue,
+        repoBackground: colorNames.white,
+        repoShadow: colorNames.violet2percent,
+        repoBorder: colorNames.iron30percent,
+        repoBorderHover: colorNames.scienceBlue20percent,
+        repoLink: colorNames.dodgerBlue,
+        repoLinkHover: colorNames.scienceBlue,
+        buttonText: colorNames.white,
+        buttonBackground: colorNames.dodgerBlue,
+        buttonBackgroundHover: colorNames.scienceBlue,
+        socialIconBackground: colorNames.mineShaft,
+        socialIconBackgroundHover: colorNames.dodgerBlue,
+        loaderBackground: colorNames.iron30percent,
+        loaderFront: colorNames.dodgerBlue
+    }
+}
+
+export const darkTheme = {
+    ...media,
+    color: {
+        textPrimary: colorNames.white,
+        textSecondary: colorNames.white,
+        background: colorNames.mineShaft,
+        sectionBackground: colorNames.mineShaft72percent,
+        sectionShadow: colorNames.violet2percent,
+        sectionTitleBorder: colorNames.iron10percent,
+        sectionBullet: colorNames.dodgerBlue,
+        repoBackground: colorNames.mineShaft72percent,
+        repoShadow: colorNames.violet2percent,
+        repoBorder: colorNames.iron10percent,
+        repoBorderHover: colorNames.scienceBlue50percent,
+        repoLink: colorNames.dodgerBlue,
+        repoLinkHover: colorNames.scienceBlue,
+        buttonText: colorNames.white,
+        buttonBackground: colorNames.dodgerBlue,
+        buttonBackgroundHover: colorNames.scienceBlue,
+        socialIconBackground: colorNames.white,
+        socialIconBackgroundHover: colorNames.dodgerBlue,
+        loaderBackground: colorNames.mineShaft72percent,
+        loaderFront: colorNames.dodgerBlue
+    }
+}
