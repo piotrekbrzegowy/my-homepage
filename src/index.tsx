59516c2165e30241dff36c711fa8147112e36f37
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-06-02 18:41:21 +0200

    added store provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './core/App/App';
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
+import store from './core/store';
+import App from './core/App/App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
