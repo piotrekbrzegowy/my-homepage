9c24ee4f99313e5577feb327d0f876c2e74aa54c
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-06-03 11:52:23 +0200

    ThemeSwitch and Skills components added to PersonalHomepage

--- src/features/homepage/PersonalHomepage/index.tsx
+++ src/features/homepage/PersonalHomepage/index.tsx
@@ -1,8 +1,13 @@
+import { ThemeSwitch } from "../../../common/ThemeSwitch"
 import { Header } from "./Header"
+import { Skills } from "./Section"
+import { skillsData } from "./skillsData"
 import { Wrapper } from "./styled"
 
 export const PersonalHomepage = () => (
     <Wrapper>
+        <ThemeSwitch />
         <Header />
+        <Skills title={"My skillset includes"} skills={skillsData} />
     </Wrapper>
 )
