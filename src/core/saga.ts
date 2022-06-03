715f21147c26a811330ae62292faa2bf98034b66
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-06-03 22:42:02 +0200

    added PersonalHomepage saga to global saga

--- src/core/saga.ts
+++ src/core/saga.ts
@@ -1,5 +1,6 @@
 import { all } from '@redux-saga/core/effects';
+import { watchFetchRepositories } from '../features/homepage/personalHomepageSaga';
 
 export default function* rootSaga() {
-  yield all([]);
+  yield all([watchFetchRepositories()]);
 }
