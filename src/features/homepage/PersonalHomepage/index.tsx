cd9e6028cc1fbe71f93bbfa32972133374f16f24
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-06-03 15:00:05 +0200

    added Subheader component to PersonalHomepage

--- src/features/homepage/PersonalHomepage/index.tsx
+++ src/features/homepage/PersonalHomepage/index.tsx
@@ -3,6 +3,7 @@ import { Header } from "./Header";
 import { Skills } from "./Section";
 import { futureSkillsData, skillsData } from "./skillsData";
 import { Wrapper } from "./styled";
+import { Subheader } from "./Subheader";
 
 export const PersonalHomepage = () => (
     <Wrapper>
@@ -10,5 +11,6 @@ export const PersonalHomepage = () => (
         <Header />
         <Skills title={"My skillset includes"} skills={skillsData} />
         <Skills title={"What I want to learn next"} skills={futureSkillsData} />
+        <Subheader title={"Portfolio"} description={"My recent projects"} />
     </Wrapper>
 )
