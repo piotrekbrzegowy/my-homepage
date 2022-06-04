352592f4c5006ccf75da6f155f0071a0e1dfeaee
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-06-04 12:39:45 +0200

    mapping repositories to Portfolio section Tiles

--- src/features/homepage/PersonalHomepage/Portfolio/index.tsx
+++ src/features/homepage/PersonalHomepage/Portfolio/index.tsx
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
-import { useAppDispatch } from "../../hooks"
-import { fetchRepositoriesLoading } from "../../personalHomepageSlice";
+import { useAppDispatch, useAppSelector } from "../../hooks"
+import { fetchRepositoriesLoading, selectRepositories } from "../../personalHomepageSlice";
 import { Description, Link, LinkDescription, LinksSection, Tile, Title, Wrapper } from "./styled"
 
 export const PortfolioSection = () => {
     const dispatch = useAppDispatch();
+    const repositories = useAppSelector(selectRepositories);
 
     useEffect(() => {
         dispatch(fetchRepositoriesLoading());
@@ -12,12 +13,14 @@ export const PortfolioSection = () => {
 
     return (
         <Wrapper>
-            <Tile>
-                <Title>title</Title>
-                <Description>description</Description>
-                <LinksSection><LinkDescription>link description</LinkDescription><Link target="_blank" rel="noreferrer" href="">LINK</Link></LinksSection>
-                <LinksSection><LinkDescription>link description</LinkDescription><Link target="_blank" rel="noreferrer" href="">LINK</Link></LinksSection>
-            </Tile>
+            {repositories?.map(({ id, name, description, homepage, html_url }) => (
+                <Tile key={id}>
+                    <Title>{name}</Title>
+                    {description && <Description>{description}</Description>}
+                    {homepage && <LinksSection><LinkDescription>Demo:</LinkDescription><Link target="_blank" rel="noreferrer" href={homepage}>{homepage}</Link></LinksSection>}
+                    <LinksSection><LinkDescription>Code:</LinkDescription><Link target="_blank" rel="noreferrer" href={html_url}>{html_url}</Link></LinksSection>
+                </Tile>
+            ))}
         </Wrapper>
     )
 }
