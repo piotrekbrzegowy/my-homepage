61cab07d99304aaaa8540f1cec7204dda5e44a30
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-06-04 13:26:31 +0200

    added StateChecker to PortfolioSection component

--- src/features/homepage/PersonalHomepage/Portfolio/index.tsx
+++ src/features/homepage/PersonalHomepage/Portfolio/index.tsx
@@ -1,26 +1,31 @@
 import { useEffect } from "react";
+import { StateChecker } from "../../../../common/StateChecker";
 import { useAppDispatch, useAppSelector } from "../../hooks"
-import { fetchRepositoriesLoading, selectRepositories } from "../../personalHomepageSlice";
+import { fetchRepositoriesLoading, selectError, selectLoading, selectRepositories } from "../../personalHomepageSlice";
 import { Description, Link, LinkDescription, LinksSection, Tile, Title, Wrapper } from "./styled"
 
 export const PortfolioSection = () => {
     const dispatch = useAppDispatch();
     const repositories = useAppSelector(selectRepositories);
+    const isLoading = useAppSelector(selectLoading);
+    const isError = useAppSelector(selectError);
 
     useEffect(() => {
         dispatch(fetchRepositoriesLoading());
     }, [dispatch])
 
     return (
-        <Wrapper>
-            {repositories?.map(({ id, name, description, homepage, html_url }) => (
-                <Tile key={id}>
-                    <Title>{name}</Title>
-                    {description && <Description>{description}</Description>}
-                    {homepage && <LinksSection><LinkDescription>Demo:</LinkDescription><Link target="_blank" rel="noreferrer" href={homepage}>{homepage}</Link></LinksSection>}
-                    <LinksSection><LinkDescription>Code:</LinkDescription><Link target="_blank" rel="noreferrer" href={html_url}>{html_url}</Link></LinksSection>
-                </Tile>
-            ))}
-        </Wrapper>
+        <StateChecker isLoading={isLoading} isError={isError}>
+            <Wrapper>
+                {repositories?.map(({ id, name, description, homepage, html_url }) => (
+                    <Tile key={id}>
+                        <Title>{name}</Title>
+                        {description && <Description>{description}</Description>}
+                        {homepage && <LinksSection><LinkDescription>Demo:</LinkDescription><Link target="_blank" rel="noreferrer" href={homepage}>{homepage}</Link></LinksSection>}
+                        <LinksSection><LinkDescription>Code:</LinkDescription><Link target="_blank" rel="noreferrer" href={html_url}>{html_url}</Link></LinksSection>
+                    </Tile>
+                ))}
+            </Wrapper>
+        </StateChecker>
     )
 }
