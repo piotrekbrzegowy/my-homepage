3ea22ed8e89513a46f8c854ee923247f4ff8c313
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-06-05 15:41:50 +0200

    styles for SocialIcons

---
+++ src/features/homepage/PersonalHomepage/Footer/SocialIcons/img/styled.ts
@@ -0,0 +1,33 @@
+import { ReactNode } from "react";
+import styled from "styled-components";
+
+export const List = styled.ul`
+    list-style-type: none;
+    padding: 0;
+    margin: 32px 0 110px 0;
+    display: flex;
+    gap: 24px;
+
+    @media(max-width: ${({ theme }) => theme.media.mobileMax}px){
+        margin: 28px 0 30px 0;
+    }
+`;
+
+export const Item = styled.li``;
+
+export const Link = styled.a`
+    color: ${({ theme }) => theme.color.socialIconBackground};
+
+    &:hover{
+    color: ${({ theme }) => theme.color.socialIconBackgroundHover};
+    }
+`;
+
+export const styledIcon = (Icon) => styled(Icon)`
+    width: 48px;
+    height: auto;
+
+    @media(max-width: ${({ theme }) => theme.media.mobileMax}px){
+        width: 32px;
+    }
+`;
