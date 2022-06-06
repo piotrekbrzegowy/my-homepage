d572dfdc7a623386ffb209b030069a11047de221
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-06-06 10:11:20 +0200

    created Footer component

---
+++ src/features/homepage/PersonalHomepage/Footer/index.tsx
@@ -0,0 +1,10 @@
+import { email } from "../links";
+import { Description, SmallText, TitleLink, Wrapper } from "./styled";
+
+export const Footer = () => (
+    <Wrapper>
+        <SmallText>LET'S TALK!</SmallText>
+        <TitleLink href={`mailto: ${email}`}>brzegowypiotrek@gmail.com</TitleLink>
+        <Description>I’m always open to new projects whenever I have the time. If you want to create a website and need some help to make your ideas come to life, feel free to contact me</Description>
+    </Wrapper>
+)
